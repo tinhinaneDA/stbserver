f05a78f93eb51e1f41ee7c17d8f94f002a758244
dahmani <dahmani.tine@gmail.com>
2020-03-24 17:09:43 +0100

    modfi

--- src/main/java/war/StbserverApplication.java
+++ src/main/java/war/StbserverApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StbserverApplication {
 
 	public static void main(String[] args) {
-		System.getProperties().put("server.port", 8100);
+		
 		SpringApplication.run(StbserverApplication.class, args);
 	}
 
